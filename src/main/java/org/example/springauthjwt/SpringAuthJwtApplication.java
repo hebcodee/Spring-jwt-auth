5a36cffd2e1d592f40aeb3cd605cf46f2efd62af
hebcodee <herberth.ferreira6@gmail.com>
2026-02-05 20:45:19 -0300

    Initial Commit

---
+++ src/main/java/org/example/springauthjwt/SpringAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package org.example.springauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthJwtApplication.class, args);
+    }
+
+}
