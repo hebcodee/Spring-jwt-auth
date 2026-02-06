80fcd9d1300fdf5517f74946a7c398d5826c1b56
hebcodee <herberth.ferreira6@gmail.com>
2026-02-05 21:17:26 -0300

    feat: Add Logs

--- src/main/java/org/example/springauthjwt/service/UserServiceImpl.java
+++ src/main/java/org/example/springauthjwt/service/UserServiceImpl.java
@@ -18,16 +18,19 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
+        log.info("New User Save Success To The Database");
         return userRepo.save(user);
     }
 
     @Override
     public User getUser(Long id) {
+        log.info("Fetching user {}", id);
         return userRepo.findById(id).orElse(null);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
